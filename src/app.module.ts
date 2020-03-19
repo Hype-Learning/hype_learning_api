3c62b14f56afe5fdca1072b7809cbab2aa3363d4
MattViper <mattoshinigami@gmail.com>
2020-03-19 22:31:30 +0100

    Add courses module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AuthzModule } from './authz/authz.module';
 import { ConfigModule } from '@nestjs/config';
+import { CoursesModule } from './courses/courses.module';
 
 
 
 @Module({
   imports: [
     AuthzModule, 
-    ConfigModule.forRoot()
+    ConfigModule.forRoot(), CoursesModule
   ],
   controllers: [],
   providers: [],
