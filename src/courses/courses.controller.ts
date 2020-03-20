f2618711d97e8e43a4a723b10cfcc3f253c74401
MattViper <mattoshinigami@gmail.com>
2020-03-20 20:00:00 +0100

    Use CoursesController AuthGuard

--- src/courses/courses.controller.ts
+++ src/courses/courses.controller.ts
@@ -1,4 +1,5 @@
-import { Controller, Post, Body, Get, Param, Delete, Put } from '@nestjs/common';
+import { Controller, Post, Body, Get, Param, Delete, Put, UseGuards } from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
 import { CoursesService } from './courses.service';
 import { CreateCourseDto } from './dto/create-course.dto';
 import { Course } from './course.entity';
@@ -10,6 +11,7 @@ export class CoursesController {
     constructor(private coursesService: CoursesService)
     {}
 
+    @UseGuards(AuthGuard('jwt'))
     @Post()
     create(@Body() createCourseDto: CreateCourseDto): Promise<Course> {
         return this.coursesService.create(createCourseDto);
@@ -21,16 +23,19 @@ export class CoursesController {
         return this.coursesService.findAll();
     }
 
+    @UseGuards(AuthGuard('jwt'))
     @Get(':id')
     findOne(@Param('id') id: string): Promise<Course> {
         return this.coursesService.findOne(id);
     }
 
+    @UseGuards(AuthGuard('jwt'))
     @Put(':id')
     update(@Param('id') id, @Body() courseData: CreateCourseDto){
         return this.coursesService.update(id, courseData);
     }
 
+    @UseGuards(AuthGuard('jwt'))
     @Delete(':id')
     remove(@Param('id') id: string): Promise<void> {
         return this.coursesService.remove(id);
