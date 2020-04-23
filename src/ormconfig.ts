4a75b5ba3ad462253aeae01bd4d8656d2ba00849
Ewelina Lasowy <ewelinalasowy@wp.pl>
2020-04-23 18:11:14 +0200

    test production

--- src/ormconfig.ts
+++ src/ormconfig.ts
@@ -49,6 +49,25 @@ if (environment !== 'production') {
     password: connectionOptions.password,
     database: connectionOptions.database,
     entities: [__dirname + '/**/*.entity{.ts,.js}'],
+
+    // We are using migrations, synchronize should be set to false.
+    synchronize: true,
+
+    // Run migrations automatically,
+    // you can disable this if you prefer running migration manually.
+    migrationsRun: false,
+    logging: true,
+    logger: 'file',
+
+    // Allow both start:prod and start:dev to use migrations
+    // __dirname is either dist or src folder, meaning either
+    // the compiled js in prod or the ts in dev.
+    migrations: [__dirname + '/migrations/**/*{.ts,.js}'],
+    cli: {
+      // Location of migration should be inside src folder
+      // to be compiled into dist/ folder.
+      migrationsDir: 'src/migrations',
+    },
   };
 }
 
